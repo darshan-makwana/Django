cddb5600d6c247b6eb2996c7e8ac21aad683bb76
Coode-It <coode-it@Coode-Its-iMac.local>
2022-02-11 18:32:28 +0530

    Your message about the commit

--- myproject/wsgi.py
+++ myproject/wsgi.py
@@ -10,6 +10,12 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+import sys
+
+# assuming your Django settings file is at '/home/myusername/mysite/mysite/settings.py'
+path = '/home/darsh27/Django/myproject'
+if path not in sys.path:
+    sys.path.insert(0, path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 
