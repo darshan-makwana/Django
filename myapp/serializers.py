096124f896c4880242d0e094aaac4303d55edc9d
Coode-It <coode-it@Coode-Its-iMac.local>
2022-02-11 18:35:36 +0530

    Your message about the commit

--- myapp/serializers.py
+++ myapp/serializers.py
@@ -3,8 +3,6 @@ from rest_framework import serializers
 from .models import *
 
 class UserInfoSerializer(serializers.ModelSerializer):
-    Username=
-
     class Meta:
         model = UserInfo
         fields = '__all__'
